21c01a5a5fac26cb68375315d4f0f0af7d042a4e
Sussyamogus2016 <Naratise@gmail.com>
2024-12-03 18:06:32 +0300

    com3

---
+++ zadanie3.py
@@ -0,0 +1,8 @@
+dict = {'apple':'яблоко', 'red':'красный', 'cat':'кот', 'sky':'небо', 'laptop':'ноутбук'}
+print('Весь словарь:')
+for key in dict:
+    print(key, dict[key])
+print()
+for i in dict.keys():
+    c = i
+print('Перевод последнего слова -',dict[c])
